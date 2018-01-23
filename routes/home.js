5bc0115208ef268edd607e81e900faa5547ce537
Robert <min.xia@quvideo.com>
2018-01-23 22:00:18 +0800

    add audio url

--- routes/home.js
+++ routes/home.js
@@ -4,10 +4,14 @@ var URL = require('url');
 var MusicModel = require('./model.js');
 
 router.get('/list',function(req,res,next){
-	var music_1 = new MusicModel('1','1','Here With Me','describe title_1',{'origin':'http://fun-image.oss-cn-beijing.aliyuncs.com/1.jpeg'},0,1,0,0,0);
-	var music_2 = new MusicModel('2','2','Love The Way You Lie','describe title_2',{
-																					'origin':'http://fun-image.oss-cn-beijing.aliyuncs.com/2.jpeg'
-},0,1,0,0,0);
+	var music_1 = new MusicModel('1','1','Here With Me','describe title_1',
+		'http://fun-music.oss-cn-beijing.aliyuncs.com/Here%20With%20Me.mp3',
+		{'origin':'http://fun-image.oss-cn-beijing.aliyuncs.com/1.jpeg'},
+		0,1,0,0,0);
+	var music_2 = new MusicModel('2','2','Love The Way You Lie','describe title_2',
+		'http://fun-music.oss-cn-beijing.aliyuncs.com/Here%20With%20Me.mp3',
+		{'origin':'http://fun-image.oss-cn-beijing.aliyuncs.com/2.jpeg'}
+		,0,1,0,0,0);
 	var params = URL.parse(req.url,true).query;
 	var response = {status:1,data:[music_1,music_2]};
 	res.send(JSON.stringify(response));
--- routes/model.js
+++ routes/model.js
@@ -12,11 +12,12 @@ function Tag(){
 	this.cover;
 }
 
-function MusicModel(vol_id,vol_number,title,summary,covers,create_time,is_free,is_trial,comments_count,favs_count){
+function MusicModel(vol_id,vol_number,title,summary,audioUrl,covers,create_time,is_free,is_trial,comments_count,favs_count){
 	this.vol_id = vol_id;
 	this.vol_number = vol_number;
 	this.title = title;
 	this.summary = summary;
+	this.audioUrl = audioUrl;
 	this.covers = covers;
 	this.create_time = create_time;
 	this.is_free = is_free;
