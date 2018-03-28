d5bba3dd9f726d73f09305ab02363973fd267dce
Robert <min.xia@quvideo.com>
2018-03-28 19:16:17 +0800

    merge

--- routes/home.js
+++ routes/home.js
@@ -52,7 +52,7 @@ router.get('/list',function(req,res,next){
 	var page = params.page;//页码
 	var size = params.pageSize;//每页个数
 	cardModel.count({},function(err,count) {
-		cardModel.find({},null,{skip:page*(size - 1),limit:size},function(err,result,res) {
+		cardModel.find({},null,{"$skip":page*(size - 1),"$limit":size},function(err,result,res) {
 			if(err)  return console.log(err);
 			console.log(result);
 			console.log(count);
